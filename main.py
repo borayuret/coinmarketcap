b02b651eb70a67437879160dda6740862dbe2755
Bora Yüret <bora.yuret@bilgeadam.com>
2021-03-06 11:31:34 +0300

    api ile veri çekebildik.

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import requests
 import json
+
+
+api_request = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency"
+                           "/listings/latest?start=1&limit=5&convert=USD&"
+                           "CMC_PRO_API_KEY=f1e4d2c9-ca98-4890-8b29-70ee91a81a86")
+
+result = json.loads(api_request.content)
+
+print(result)
+

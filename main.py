2b024d0b20b71fead29787b0414ef874538fc9f2
Bora Yüret <bora.yuret@bilgeadam.com>
2021-04-11 12:44:29 +0300

    coinmarketcap GUI+DB son hali

--- main.py
+++ main.py
@@ -140,7 +140,7 @@ def app_menu():
 def my_portfolio():
     api_request = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency"
                                "/listings/latest?start=1&limit=10&convert=USD&"
-                               "CMC_PRO_API_KEY=f1e4d2c9-ca98-4890-8b29-70ee91a81a86")
+                               "CMC_PRO_API_KEY=f1e4d2c9-ca98-4890-8b29-70ee91a81a87")
 
     result = json.loads(api_request.content)
 
